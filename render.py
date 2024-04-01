8339e4cfb373bc1d32fd76ae6ab785b95b118ab4
Your Name <you@example.com>
2024-04-01 10:47:03 +0900

    commit

--- render.py
+++ render.py
@@ -21,18 +21,22 @@ from argparse import ArgumentParser
 from arguments import ModelParams, PipelineParams, get_combined_args
 from gaussian_renderer import GaussianModel
 
-def render_set(model_path, name, iteration, views, gaussians, pipeline, background):
-    render_path = os.path.join(model_path, name, "ours_{}".format(iteration), "renders")
-    gts_path = os.path.join(model_path, name, "ours_{}".format(iteration), "gt")
+import nvtx
 
-    makedirs(render_path, exist_ok=True)
-    makedirs(gts_path, exist_ok=True)
+@nvtx.annotate("render_set", color="yellow")
+def render_set(model_path, name, iteration, views, gaussians, pipeline, background):
+    # render_path = os.path.join(model_path, name, "ours_{}".format(iteration), "renders")
+    # gts_path = os.path.join(model_path, name, "ours_{}".format(iteration), "gt")
+
+    # makedirs(render_path, exist_ok=True)
+    # makedirs(gts_path, exist_ok=True)
 
     for idx, view in enumerate(tqdm(views, desc="Rendering progress")):
         rendering = render(view, gaussians, pipeline, background)["render"]
-        gt = view.original_image[0:3, :, :]
-        torchvision.utils.save_image(rendering, os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
-        torchvision.utils.save_image(gt, os.path.join(gts_path, '{0:05d}'.format(idx) + ".png"))
+        
+        # gt = view.original_image[0:3, :, :]
+        # torchvision.utils.save_image(rendering, os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
+        # torchvision.utils.save_image(gt, os.path.join(gts_path, '{0:05d}'.format(idx) + ".png"))
 
 def render_sets(dataset : ModelParams, iteration : int, pipeline : PipelineParams, skip_train : bool, skip_test : bool):
     with torch.no_grad():
